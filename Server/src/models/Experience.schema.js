c65e579127ba5a4278db8fe6df06890af1f3f720
faissal17 <faissalaoukacha@gmail.com>
2024-02-28 11:03:37 +0100

    create experience schema

---
+++ Server/src/models/Experience.schema.js
@@ -0,0 +1,54 @@
+const mongoose = require('express');
+
+const experienceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default:
+      'https://res.cloudinary.com/dmhcnhtng/image/upload/v1643044376/avatars/default_pic_jeaybr.png',
+  },
+  website: {
+    type: String,
+    required: true,
+  },
+  earning: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  tags: [String],
+  location: {
+    type: String,
+    required: true,
+  },
+  feedback: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment',
+  },
+  like: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Like',
+  },
+  status: {
+    type: String,
+    enum: ['excellent', 'good', 'not-recommended'],
+  },
+});
+
+const Experience = mongoose.model('Experience', experienceSchema);
+
+module.exports = Experience;
