957be7023762f43299fa7a80fda880f4ef02205f
faissal17 <faissalaoukacha@gmail.com>
2024-03-12 15:04:48 +0000

    create a post component

--- client/src/components/Experiences/Home.jsx
+++ client/src/components/Experiences/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ExperienceNavbar from '../../shared/ExperienceShared/ExperienceNavbar';
-
+import Post from '../../shared/ExperienceShared/Post';
 function Home() {
-  return <ExperienceNavbar />;
+  return (
+    <React.Fragment>
+      <ExperienceNavbar />
+      
+      <Post />
+    </React.Fragment>
+  );
 }
 
 export default Home;
--- client/src/shared/ExperienceShared/ExperienceNavbar.jsx
+++ client/src/shared/ExperienceShared/ExperienceNavbar.jsx
@@ -32,6 +32,7 @@ export default function ExperienceNavbar() {
                   )}
                 </Disclosure.Button>
               </div>
+
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <img className="h-8 w-auto" src={Logo} alt="Your Company" />
@@ -55,6 +56,30 @@ export default function ExperienceNavbar() {
                     ))}
                   </div>
                 </div>
+
+                <form class="w-96 mx-auto">
+                  <label
+                    for="default-search"
+                    class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                  >
+                    Search
+                  </label>
+                  <div class="relative">
+                    <input
+                      type="search"
+                      id="default-search"
+                      class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Search for other's experiences"
+                      required
+                    />
+                    <button
+                      type="submit"
+                      class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Search
+                    </button>
+                  </div>
+                </form>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
