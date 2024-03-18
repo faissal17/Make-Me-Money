43e58561a0797d94d62cb104449af1139e72cff3
faissal17 <faissalaoukacha@gmail.com>
2024-03-18 11:35:08 +0000

    get the user who created the probleme

--- Server/src/controllers/probleme/getAllProbleme.controller.js
+++ Server/src/controllers/probleme/getAllProbleme.controller.js
@@ -2,7 +2,7 @@ const Probleme = require('../../models/Probleme.schema');
 
 const getAllProbleme = async (req, res) => {
   try {
-    const probleme = await Probleme.find();
+    const probleme = await Probleme.find().populate('user');
     if (!probleme) {
       return res.status(400).json({ message: 'No probleme are found' });
     }
