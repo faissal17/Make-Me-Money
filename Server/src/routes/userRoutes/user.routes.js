0cc3fd53f5e1c0c05b884fbcab6fa852526bc99b
faissal17 <faissalaoukacha@gmail.com>
2024-03-21 15:34:20 +0000

    update profile user is done

--- Server/src/routes/userRoutes/user.routes.js
+++ Server/src/routes/userRoutes/user.routes.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const usersRoutes = express.Router();
+const multer = require('multer');
+
+const { checkAuth } = require('../../middlewares/authMiddleware');
 
 const {
   getAllUsers,
   getCurrentUser,
 } = require('../../controllers/user/getAllUser.controller');
-const { checkAuth } = require('../../middlewares/authMiddleware');
+const updateUser = require('../../controllers/user/updateUser.controller');
 
 usersRoutes.route('/').get(getAllUsers);
 usersRoutes.route('/current').get(checkAuth, getCurrentUser);
+
+const upload = multer({ dest: 'uploads/' });
+
+usersRoutes.route('/:id').put(upload.single('image'), updateUser);
 
 module.exports = usersRoutes;
